c8e1882be662753a24adf828701786a0c21714e4
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-05 10:57:35 +0700

    Review on movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_211814) do
+ActiveRecord::Schema.define(version: 2018_06_04_213520) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_211814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
