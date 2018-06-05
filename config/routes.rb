7a39dd35df471929f87b199c02faa564bb41dc9a
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-05 10:57:35 +0700

    Review on movie

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
-  devise_for :users
   root 'movies#index'
 
-  resources :movies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :movies do
+  	resources :reviews, except: [:show, :index]
+  end
 end
